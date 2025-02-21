39262694a009fe12d188aad53ac61b36226d3533
PeiDong Yan <31368606+Mrytn@users.noreply.github.com>
2025-02-21 17:14:26 +0800

    1

--- code/chapter-1/01-hello-pytorch.py
+++ code/chapter-1/01-hello-pytorch.py
@@ -9,7 +9,7 @@
 import torch
 
 print("Hello World, Hello PyTorch {}".format(torch.__version__))
-
+# print("\nCUDA is available: {}".format(torch.cuda.is_available()))
 print("\nCUDA is available:{}, version is {}".format(torch.cuda.is_available(), torch.version.cuda))
 
-print("\ndevice_name: {}".format(torch.cuda.get_device_name(0)))
+# print("\ndevice_name: {}".format(torch.cuda.get_device_name(0)))
