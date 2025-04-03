58e93923f47c8db512a90af570cd04c818f8927a
PeiDong Yan <31368606+Mrytn@users.noreply.github.com>
2025-04-03 17:29:52 +0800

    ed-cp6.4

--- code/chapter-6/04_cam_series_demo.py
+++ code/chapter-6/04_cam_series_demo.py
@@ -16,13 +16,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torchvision.models as models
-
 from pytorch_grad_cam import GradCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
 from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from pytorch_grad_cam.utils.image import show_cam_on_image
 from torchvision.models import resnet50
 from matplotlib import pyplot as plt
-
 def img_transform(img_in, transform):
     """
     将img进行预处理，并转换成模型输入所需的形式—— B*C*H*W
@@ -34,8 +32,6 @@ def img_transform(img_in, transform):
     img = transform(img)
     img = img.unsqueeze(0)    # C*H*W --> B*C*H*W
     return img
-
-
 def img_preprocess(img_in):
     """
     读取图片，转为模型可读的形式
@@ -51,45 +47,59 @@ def img_preprocess(img_in):
     ])
     img_input = img_transform(img, transform)
     return img_input
-
-
 def cam_factory(cam_name_):
+    """
+    根据字符串动态创建 CAM 对象
+    :param cam_name_: str, CAM 方法名称
+    :return: 对应的 CAM 实例
+    """
+    # 例如 cam_factory("GradCAM") 会执行 GradCAM 这个类，并返回其实例。
+    # eval 可能导致安全问题（代码执行风险），建议使用 getattr 替代
+    # getattr(cam_modules, cam_name_)()
     return eval(cam_name_)
 
 
 if __name__ == '__main__':
-
-    path_img = "both.png"
+    path_img = "code/chapter-6/both.png"
     output_dir = "./Result"
-
     # 图片读取
-    img = cv2.imread(path_img, 1)  # H*W*C
-    img_input = img_preprocess(img)
-
-    model = resnet50(pretrained=True)
-    target_layers = [model.layer4[-1]]
+    img = cv2.imread(path_img, 1)  # 读取彩色图片 (H*W*C)
+    img_input = img_preprocess(img)  # 预处理图片
+    model = resnet50(pretrained=True)  # 加载 ResNet50 预训练模型
+    target_layers = [model.layer4[-1]]  # 选取 ResNet50 的最后一层作为目标层
     input_tensor = img_input
-
-    cam_alg_list = "GradCAM,ScoreCAM,GradCAMPlusPlus,XGradCAM,EigenCAM,FullGrad".split(",")
-
-
-
-
+    cam_alg_list = "GradCAM,ScoreCAM,GradCAMPlusPlus,XGradCAM,EigenCAM,FullGrad".split(
+        ",")
     plt.tight_layout()
     # fig, axs = plt.subplots(2, 3, figsize=(9, 9))
     fig, axs = plt.subplots(2, 3)
     for idx, cam_name in enumerate(cam_alg_list):
+        # # 创建 CAM 实例
         cam = cam_factory(cam_name)(model=model, target_layers=target_layers)
         # targets = [e.g ClassifierOutputTarget(281)]
         # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+        # # 计算 CAM 可视化结果
+        # targets 通常指的是你希望计算激活图时关注的特定类别。none不关注特定类别
+        # # 取出第一张图片的结果
         grayscale_cam = cam(input_tensor=input_tensor, targets=None)  # If targets is None, the highest scoring category
         # In this example grayscale_cam has only one image in the batch:
         grayscale_cam = grayscale_cam[0, :]
         # img_norm = cv2.normalize(img, None, 0, 1, cv2.NORM_MINMAX)
+        #  # 归一化原始图片
         img_norm = img/255.
-        visualization = show_cam_on_image(img_norm, grayscale_cam, use_rgb=False)
+        # 将 CAM 热力图（grayscale_cam）叠加到归一化后的原始图像（img_norm）上，生成带有热力图的图像。
+        # img_norm：归一化后的原始图像（形状为 [H, W, C]，像素值在 0 到 1 之间）。
+# grayscale_cam：计算得到的 CAM 热力图（一般为灰度图，形状为 [H, W]，表示每个像素点的关注程度）。
+# use_rgb=False：指定是否将生成的热力图渲染为 RGB 格式。因为 grayscale_cam 是灰度图，通常不需要进行颜色转换，所以设置为 False，而是直接使用灰度热力图进行叠加
+# grayscale_cam 是一个灰度图，它表示图像中每个像素点的关注程度，但仅仅是一个二维的灰度图（即单通道图像），不包含颜色信息。
+# 然而，show_cam_on_image 函数并不是简单地将这个灰度热力图直接叠加到原图上，而是会使用一定的色彩映射来将热力图的数值映射到颜色空间中，从而生成一个“高亮”效果的图像。这种高亮效果表现为在热力图高值区域（即模型特别关注的区域）使用较为鲜艳的颜色（如红色或黄色），而在低值区域则使用较暗的颜色。
+        visualization = show_cam_on_image(img_norm, grayscale_cam,
+                                          use_rgb=False)
+        # 将图像从 BGR 色彩空间转换为 RGB 色彩空间。
         vis_rgb = cv2.cvtColor(visualization, cv2.COLOR_BGR2RGB)
-
+        # axs 是一个包含多个子图的 2D 数组（例如 2x3 子图，表示 2 行 3 列的排列）。
+# ravel() 将 axs 数组展平为 1D 数组，方便通过索引来访问子图。
+# [idx] 是当前子图的索引，imshow(vis_rgb) 将热力图显示在该子图
         im = axs.ravel()[idx].imshow(vis_rgb)
         axs.ravel()[idx].set_title(cam_name)
     plt.show()
--- code/chapter-6/04_grad_cam.py
+++ code/chapter-6/04_grad_cam.py
@@ -15,19 +15,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms as transforms
 import torchvision.models as models
-
-
 def load_class_names(p_clsnames):
     """
     加载标签名
     :param p_clsnames:
     :return:
     """
+    # r表示以只读打开
     with open(p_clsnames, "r") as f:
         class_names = json.load(f)
     return class_names
-
-
 def img_transform(img_in, transform):
     """
     将img进行预处理，并转换成模型输入所需的形式—— B*C*H*W
@@ -39,8 +36,6 @@ def img_transform(img_in, transform):
     img = transform(img)
     img = img.unsqueeze(0)    # C*H*W --> B*C*H*W
     return img
-
-
 def img_preprocess(img_in):
     """
     读取图片，转为模型可读的形式
@@ -52,34 +47,35 @@ def img_preprocess(img_in):
     img = img[:, :, ::-1]   # BGR --> RGB
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize([0.4948052, 0.48568845, 0.44682974], [0.24580306, 0.24236229, 0.2603115])
+        transforms.Normalize([0.4948052, 0.48568845, 0.44682974], [
+                             0.24580306, 0.24236229, 0.2603115])
     ])
     img_input = img_transform(img, transform)
     return img_input
-
-
 def backward_hook(module, grad_in, grad_out):
+    # detach() 是 PyTorch 张量的一个方法，其作用是创建一个与原张量共享数据，但不参与计算图构建的新张量
     grad_block.append(grad_out[0].detach())
-
-
 def farward_hook(module, input, output):
     fmap_block.append(output)
-
-
+# 显示并保存 CAM
 def show_cam_on_image(img, mask, out_dir):
-    heatmap = cv2.applyColorMap(np.uint8(255*mask), cv2.COLORMAP_JET)
-    heatmap = np.float32(heatmap) / 255
-    cam = heatmap + np.float32(img)
-    cam = cam / np.max(cam)
-
+    #  # 生成热力图
+    # OpenCV 提供的 伪彩色映射，使用 JET 颜色映射。
+    # 红色区域表示高权重，蓝色区域表示低权重。
+    heatmap = cv2.applyColorMap(
+        np.uint8(255 * mask), cv2.COLORMAP_JET)  # 生成热力图
+    heatmap = np.float32(heatmap) / 255  # 归一化
+    # img 是原始输入图像（已归一化到 [0,1]）。
+# heatmap 是伪彩色 CAM。
+# 逐元素相加，得到 叠加后的可视化结果
+    cam = heatmap + np.float32(img)  # 叠加原图
+    cam = cam / np.max(cam)  # 归一化
     path_cam_img = os.path.join(out_dir, "cam.jpg")
     path_raw_img = os.path.join(out_dir, "raw.jpg")
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     cv2.imwrite(path_cam_img, np.uint8(255 * cam))
     cv2.imwrite(path_raw_img, np.uint8(255 * img))
-
-
 def comp_class_vec(ouput_vec, index=None):
     """
     计算类向量
@@ -88,18 +84,22 @@ def comp_class_vec(ouput_vec, index=None):
     :return: tensor
     """
     if not index:
+        #  # 若无指定类别，则选择最高概率的类别
         index = np.argmax(ouput_vec.cpu().data.numpy())
     else:
         index = np.array(index)
+    print("shape1", index.shape)
+    # # 变成 (1, 1) 形状
     index = index[np.newaxis, np.newaxis]
+    print("shape2", index.shape)
     index = torch.from_numpy(index)
+    print("shape3", index.shape)
+    # 形状 (1, 1000)，其中 index 位置是 1，其余是 0。
     one_hot = torch.zeros(1, 1000).scatter_(1, index, 1)
     one_hot.requires_grad = True
+    print("shape4", ouput_vec.shape)
     class_vec = torch.sum(one_hot * ouput_vec)  # one_hot = 11.8605
-
     return class_vec
-
-
 def gen_cam(feature_map, grads):
     """
     依据梯度和特征图，生成cam
@@ -107,56 +107,53 @@ def gen_cam(feature_map, grads):
     :param grads: np.array， in [C, H, W]
     :return: np.array, [H, W]
     """
-    cam = np.zeros(feature_map.shape[1:], dtype=np.float32)  # cam shape (H, W)
-
-    weights = np.mean(grads, axis=(1, 2))  #
-
+    cam = np.zeros(feature_map.shape[1:], dtype=np.float32)  # 初始化 CAM（H, W）
+    weights = np.mean(grads, axis=(1, 2))  # 计算每个通道的全局平均梯度
     for i, w in enumerate(weights):
-        cam += w * feature_map[i, :, :]
-
-    cam = np.maximum(cam, 0)
-    cam = cv2.resize(cam, (224, 224))
+        cam += w * feature_map[i, :, :]  # 加权求和
+    cam = np.maximum(cam, 0)  # ReLU 操作，去除负值
+    cam = cv2.resize(cam, (224, 224))  # 调整大小
     cam -= np.min(cam)
-    cam /= np.max(cam)
-
+    cam /= np.max(cam)  # 归一化
     return cam
-
-
 if __name__ == '__main__':
-
-    path_img = "both.png"
-    path_cls_names = "imagenet1000.json"
-    output_dir = "./Result"
+    path_img = "code/chapter-6/both.png"
+    path_cls_names = "code/chapter-6/imagenet1000.json"
+    output_dir = "Result"
     input_size = 224
-
+    # ImageNet 1000 类别名称的 JSON 文件
     classes = load_class_names(path_cls_names)
+    # 加载了 预训练 的 ResNet-50 模型，用于分类任务
     resnet_50 = models.resnet50(pretrained=True)
-
-    fmap_block = []
-    grad_block = []
-
+    fmap_block = []  # 存储前向传播的特征图
+    grad_block = []  # 存储反向传播的梯度
     # 图片读取
     img = cv2.imread(path_img, 1)  # H*W*C
     img_input = img_preprocess(img)
-
     # 注册hook
+    # 存储特征图（网络前向传播时的输出）
     resnet_50.layer4[-1].register_forward_hook(farward_hook)
+    # 存储梯度（损失函数对特征图的梯度）
     resnet_50.layer4[-1].register_full_backward_hook(backward_hook)
-
     # forward
-    output = resnet_50(img_input)
-    idx = np.argmax(output.cpu().data.numpy())
-    print("predict: {}".format(classes[idx]))
+    output = resnet_50(img_input)  # 执行 ResNet-50 前向传播
+    idx = np.argmax(output.cpu().data.numpy())  # 取概率最大的类别索引
+    print("predict: {}".format(classes[idx]))  # 输出预测类别
     # backward
-    resnet_50.zero_grad()
-    class_loss = comp_class_vec(output)
-    class_loss.backward()
-
-    # 生成cam
-    grads_val = grad_block[0].cpu().data.numpy().squeeze()
-    fmap = fmap_block[0].cpu().data.numpy().squeeze()
+    resnet_50.zero_grad()  # 清空梯度
+    class_loss = comp_class_vec(output)  # 计算目标类别的损失
+    class_loss.backward()  # 反向传播
+    # 提取梯度和特征图
+    grads_val = grad_block[0].cpu().data.numpy().squeeze()  # 取出梯度
+    grads_val2 = grad_block[0].cpu().data.numpy()  # 取出梯度
+    fmap = fmap_block[0].cpu().data.numpy().squeeze()  # 取出特征图
+    fmap2 = fmap_block[0].cpu().data.numpy()  # 取出特征图
+    print("f1: ", fmap.shape)
+    print("f2: ", fmap2.shape)
+    print("g1: ", grads_val.shape)
+    print("f2: ", grads_val2.shape)
+    # # 计算 CAM
     cam = gen_cam(fmap, grads_val)
-
     # 保存cam图片
     img_show = np.float32(cv2.resize(img, (input_size, input_size))) / 255
     show_cam_on_image(img_show, cam, output_dir)
