e2988cc988bbb53e7d84926d2c8e280a16879d30
PeiDong Yan <31368606+Mrytn@users.noreply.github.com>
2025-04-10 14:57:44 +0800

    ed-cp7

--- code/chapter-7/07_lenet_ensemble.py
+++ code/chapter-7/07_lenet_ensemble.py
@@ -16,30 +16,33 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from torchensemble.utils import set_module
 from torchensemble.voting import VotingClassifier
-
-
-classes = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
-
-
+classes = ['plane', 'car', 'bird', 'cat', 'deer',
+           'dog', 'frog', 'horse', 'ship', 'truck']
 def get_args_parser(add_help=True):
     import argparse
-
-    parser = argparse.ArgumentParser(description="PyTorch Classification Training", add_help=add_help)
-
-    parser.add_argument("--data-path", default=r"F:\pytorch-tutorial-2nd\data\datasets\cifar10-office", type=str, help="dataset path")
-    parser.add_argument("--model", default="resnet8", type=str, help="model name")
-    parser.add_argument("--device", default="cuda", type=str, help="device (Use cuda or cpu Default: cuda)")
+    parser = argparse.ArgumentParser(
+        description="PyTorch Classification Training", add_help=add_help)
+    parser.add_argument(
+        "--data-path", default=r"F:\pytorch-tutorial-2nd\data\datasets\cifar10-office", type=str, help="dataset path")
+    parser.add_argument("--model", default="resnet8",
+                        type=str, help="model name")
+    parser.add_argument("--device", default="cuda", type=str,
+                        help="device (Use cuda or cpu Default: cuda)")
     parser.add_argument(
         "-b", "--batch-size", default=128, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
     )
-    parser.add_argument("--epochs", default=200, type=int, metavar="N", help="number of total epochs to run")
+    parser.add_argument("--epochs", default=200, type=int,
+                        metavar="N", help="number of total epochs to run")
     parser.add_argument(
         "-j", "--workers", default=4, type=int, metavar="N", help="number of data loading workers (default: 16)"
     )
     parser.add_argument("--opt", default="SGD", type=str, help="optimizer")
-    parser.add_argument("--random-seed", default=42, type=int, help="random seed")
-    parser.add_argument("--lr", default=0.1, type=float, help="initial learning rate")
-    parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
+    parser.add_argument("--random-seed", default=42,
+                        type=int, help="random seed")
+    parser.add_argument("--lr", default=0.1, type=float,
+                        help="initial learning rate")
+    parser.add_argument("--momentum", default=0.9,
+                        type=float, metavar="M", help="momentum")
     parser.add_argument(
         "--wd",
         "--weight-decay",
@@ -49,16 +52,19 @@ def get_args_parser(add_help=True):
         help="weight decay (default: 1e-4)",
         dest="weight_decay",
     )
-    parser.add_argument("--lr-step-size", default=80, type=int, help="decrease lr every step-size epochs")
-    parser.add_argument("--lr-gamma", default=0.1, type=float, help="decrease lr by a factor of lr-gamma")
-    parser.add_argument("--print-freq", default=80, type=int, help="print frequency")
-    parser.add_argument("--output-dir", default="./Result", type=str, help="path to save outputs")
-    parser.add_argument("--resume", default="", type=str, help="path of checkpoint")
-    parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="start epoch")
-
+    parser.add_argument("--lr-step-size", default=80, type=int,
+                        help="decrease lr every step-size epochs")
+    parser.add_argument("--lr-gamma", default=0.1, type=float,
+                        help="decrease lr by a factor of lr-gamma")
+    parser.add_argument("--print-freq", default=80,
+                        type=int, help="print frequency")
+    parser.add_argument("--output-dir", default="./Result",
+                        type=str, help="path to save outputs")
+    parser.add_argument("--resume", default="", type=str,
+                        help="path of checkpoint")
+    parser.add_argument("--start-epoch", default=0, type=int,
+                        metavar="N", help="start epoch")
     return parser
-
-
 def main(args):
     device = args.device
     data_dir = args.data_path
@@ -66,9 +72,7 @@ def main(args):
     # ------------------------------------  log ------------------------------------
     logger, log_dir = utils.make_logger(result_dir)
     writer = SummaryWriter(log_dir=log_dir)
-
     # ------------------------------------ step1: dataset ------------------------------------
-
     normMean = [0.4948052, 0.48568845, 0.44682974]
     normStd = [0.24580306, 0.24236229, 0.2603115]
     normTransform = transforms.Normalize(normMean, normStd)
@@ -78,21 +82,21 @@ def main(args):
         transforms.ToTensor(),
         normTransform
     ])
-
     valid_transform = transforms.Compose([
         transforms.ToTensor(),
         normTransform
     ])
-
     # root变量下需要存放cifar-10-python.tar.gz 文件
     # cifar-10-python.tar.gz可从 "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz" 下载
-    train_set = torchvision.datasets.CIFAR10(root=data_dir, train=True, transform=train_transform, download=True)
-    test_set = torchvision.datasets.CIFAR10(root=data_dir, train=False, transform=valid_transform, download=True)
-
+    train_set = torchvision.datasets.CIFAR10(
+        root=data_dir, train=True, transform=train_transform, download=True)
+    test_set = torchvision.datasets.CIFAR10(
+        root=data_dir, train=False, transform=valid_transform, download=True)
     # 构建DataLoder
-    train_loader = DataLoader(dataset=train_set, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
-    valid_loader = DataLoader(dataset=test_set, batch_size=args.batch_size, num_workers=args.workers)
-
+    train_loader = DataLoader(
+        dataset=train_set, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+    valid_loader = DataLoader(
+        dataset=test_set, batch_size=args.batch_size, num_workers=args.workers)
     # ------------------------------------ tep2: model ------------------------------------
     model_base = utils.resnet20()
     # model_base = utils.LeNet5()
@@ -100,112 +104,114 @@ def main(args):
                        classes=classes, writer=writer, save_dir=log_dir)
     model.set_optimizer(args.opt, lr=args.lr, weight_decay=args.weight_decay)
     model.fit(train_loader, test_loader=valid_loader, epochs=args.epochs)
-
-
+# 自定义模型集成类分类器
 class MyEnsemble(VotingClassifier):
     def __init__(self, **kwargs):
         # logger, device, args, classes, writer
-        super(VotingClassifier, self).__init__(kwargs["estimator"], kwargs["n_estimators"])
+        super(VotingClassifier, self).__init__(
+            kwargs["estimator"], kwargs["n_estimators"])
         self.logger = kwargs["logger"]
         self.writer = kwargs["writer"]
         self.device = kwargs["device"]
         self.args = kwargs["args"]
         self.classes = kwargs["classes"]
         self.save_dir = kwargs["save_dir"]
-
     @staticmethod
     def save(model, save_dir, logger):
         """Implement model serialization to the specified directory."""
+        # 如果没有提供路径，就默认保存在当前目录；若路径不存在，就创建它
         if save_dir is None:
             save_dir = "./"
-
         if not os.path.isdir(save_dir):
             os.mkdir(save_dir)
-
         # Decide the base estimator name
+        # 判断是类还是对象
         if isinstance(model.base_estimator_, type):
             base_estimator_name = model.base_estimator_.__name__
         else:
             base_estimator_name = model.base_estimator_.__class__.__name__
-
         # {Ensemble_Model_Name}_{Base_Estimator_Name}_{n_estimators}
+        # 拼接文件名：
         filename = "{}_{}_{}_ckpt.pth".format(
             type(model).__name__,
             base_estimator_name,
             model.n_estimators,
         )
-
         # The real number of base estimators in some ensembles is not same as
         # `n_estimators`.
+        # 保存内容打包：
         state = {
             "n_estimators": len(model.estimators_),
             "model": model.state_dict(),
             "_criterion": model._criterion,
         }
+        # 真的保存到文件：
         save_dir = os.path.join(save_dir, filename)
-
         logger.info("Saving the model to `{}`".format(save_dir))
-
         # Save
         torch.save(state, save_dir)
-
         return
+    # 集成学习训练框架中 fit() 方法的完整实现。它的作用是训练多个子模型（即基模型），并保存验证集上表现最好的集成模型
+# train_loader: 训练数据加载器
+# epochs: 训练轮数
+# log_interval: 日志打印间隔（虽然代码中并未使用）
+# test_loader: 验证集加载器
+# save_model: 是否保存模型
+# save_dir: 模型保存路径
 
     def fit(self, train_loader, epochs=100, log_interval=100, test_loader=None, save_model=True, save_dir=None,):
-
         # 模型、优化器、学习率调整器、评估器 列表创建
         estimators = []
+        # 创建多个基模型
         for _ in range(self.n_estimators):
             estimators.append(self._make_estimator())
-
+        # 分别为每个基模型设置独立的优化器和学习率调度器。
         optimizers = []
         schedulers = []
         for i in range(self.n_estimators):
-            optimizers.append(set_module.set_optimizer(estimators[i],
-                                                       self.optimizer_name, **self.optimizer_args))
-            scheduler_ = torch.optim.lr_scheduler.MultiStepLR(optimizers[i], milestones=[100, 150],
-                                                              gamma=self.args.lr_gamma)  # 设置学习率下降策略
+            # 为第 i 个估计器（模型）设置优化器，然后将其添加到 optimizers 列表中。
+            optimizers.append(set_module.set_optimizer(
+                estimators[i], self.optimizer_name, **self.optimizer_args))
+            # MultiStepLR 按设定 epoch 降低学习率，例如在第 100 和 150 个 epoch 减小。
+            scheduler_ = torch.optim.lr_scheduler.MultiStepLR(
+                optimizers[i], milestones=[100, 150], gamma=self.args.lr_gamma)  # 设置学习率下降策略
             # scheduler_ = torch.optim.lr_scheduler.StepLR(optimizers[i], step_size=self.args.lr_step_size,
             #                                             gamma=self.args.lr_gamma)  # 设置学习率下降策略
             schedulers.append(scheduler_)
-
         acc_metrics = []
         for i in range(self.n_estimators):
             acc_metrics.append(torchmetrics.Accuracy())
-
         self._criterion = nn.CrossEntropyLoss()
-
         # epoch循环迭代
         best_acc = 0.
         for epoch in range(epochs):
-
             # training
             for model_idx, (estimator, optimizer, scheduler) in enumerate(zip(estimators, optimizers, schedulers)):
+                # 调用你自己封装的训练函数返回损失和准确率。
                 loss_m_train, acc_m_train, mat_train = \
                     utils.ModelTrainerEnsemble.train_one_epoch(
                         train_loader, estimator, self._criterion, optimizer, scheduler, epoch,
                         self.device, self.args, self.logger, self.classes)
                 # 学习率更新
                 scheduler.step()
-
                 # 记录
                 self.writer.add_scalars('Loss_group', {'train_loss_{}'.format(model_idx):
-                                                           loss_m_train.avg}, epoch)
+                                                       loss_m_train.avg}, epoch)
                 self.writer.add_scalars('Accuracy_group', {'train_acc_{}'.format(model_idx):
-                                                               acc_m_train.avg}, epoch)
-                self.writer.add_scalar('learning rate', scheduler.get_last_lr()[0], epoch)
-
-
+                                                           acc_m_train.avg}, epoch)
+                self.writer.add_scalar(
+                    'learning rate', scheduler.get_last_lr()[0], epoch)
             # validate
+            # 验证集评估整个集成模型
+            # 用多个子模型评估验证集，可能是投票结果或者平均预测。
             loss_valid_meter, acc_valid, top1_group, mat_valid = \
-                utils.ModelTrainerEnsemble.evaluate(test_loader, estimators, self._criterion, self.device, self.classes)
-
+                utils.ModelTrainerEnsemble.evaluate(
+                    test_loader, estimators, self._criterion, self.device, self.classes)
             # 日志
             self.writer.add_scalars('Loss_group', {'valid_loss':
-                                                       loss_valid_meter.avg}, epoch)
+                                                   loss_valid_meter.avg}, epoch)
             self.writer.add_scalars('Accuracy_group', {'valid_acc':
-                                                           acc_valid*100}, epoch)
-
+                                                       acc_valid*100}, epoch)
             self.logger.info(
                 'Epoch: [{:0>3}/{:0>3}]  '
                 'Train Loss avg: {loss_train:>6.4f}  '
@@ -215,24 +221,17 @@ class MyEnsemble(VotingClassifier):
                 'LR: {lr}'.format(
                     epoch, self.args.epochs, loss_train=loss_m_train.avg, loss_valid=loss_valid_meter.avg,
                     top1_train=acc_m_train.avg, top1_valid=acc_valid, lr=schedulers[0].get_last_lr()[0]))
-
             for model_idx, top1_meter in enumerate(top1_group):
-                self.writer.add_scalars('Accuracy_group', {'valid_acc_{}'.format(model_idx): top1_meter.compute()*100}, epoch)
-
+                self.writer.add_scalars('Accuracy_group', {'valid_acc_{}'.format(
+                    model_idx): top1_meter.compute()*100}, epoch)
             if acc_valid > best_acc:
                 best_acc = acc_valid
                 self.estimators_ = nn.ModuleList()
                 self.estimators_.extend(estimators)
                 if save_model:
                     self.save(self, self.save_dir, self.logger)
-
-
 if __name__ == "__main__":
     args = get_args_parser().parse_args()
     utils.setup_seed(args.random_seed)
     args.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     main(args)
-
-
-
-
--- code/chapter-7/07_model_ensemble.py
+++ code/chapter-7/07_model_ensemble.py
@@ -5,35 +5,32 @@
 @date       : 2022-06-30
 @brief      : 模型集成
 """
-
 import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
-
 from torchensemble.fusion import FusionClassifier
 from torchensemble.voting import VotingClassifier
 from torchensemble.bagging import BaggingClassifier
 from torchensemble.gradient_boosting import GradientBoostingClassifier
 from torchensemble.snapshot_ensemble import SnapshotEnsembleClassifier
 from torchensemble.soft_gradient_boosting import SoftGradientBoostingClassifier
-
 from torchensemble.utils.logging import set_logger
-
-
+# 记录模型结果
+# 将多个模型的测试结果（方法名、训练时间、评估时间、准确率）以美观的格式写入日志中，方便统一查看和比较
 def display_records(records, logger):
+    # 这是一段格式化字符串，定义了输出日志的格式：
+    # {: < 28} 表示左对齐，占 28 个字符的宽度（用来放方法名 method）。
+    # {: .2f} 表示浮点数保留两位小数
     msg = (
         "{:<28} | Testing Acc: {:.2f} % | Training Time: {:.2f} s |"
         " Evaluating Time: {:.2f} s"
     )
-
     print("\n")
     for method, training_time, evaluating_time, acc in records:
         logger.info(msg.format(method, acc, training_time, evaluating_time))
-
-
 class LeNet5(nn.Module):
     def __init__(self):
         super(LeNet5, self).__init__()
@@ -43,7 +40,6 @@ class LeNet5(nn.Module):
         self.fc1 = nn.Linear(400, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
@@ -55,22 +51,24 @@ class LeNet5(nn.Module):
 
 
 if __name__ == "__main__":
-
     # Hyper-parameters
+    # n_estimators: 如果你用的是集成方法，比如 AdaBoost 或 Bagging，这个表示子模型的数量。
+    # lr: 学习率（Learning Rate），控制每次参数更新的步长。
+    # weight_decay: 权重衰减（L2正则化），防止过拟合。
+    # epochs: 总训练轮数。
     n_estimators = 5
     lr = 1e-3
     weight_decay = 5e-4
     epochs = 100
-
     # Utils
-    data_dir = r"F:\pytorch-tutorial-2nd\data\datasets\cifar10-office"
+    data_dir = r"D:\ai\pytorch\l-PyTorch-Tutorial-2nd\data\datasets\cifar10-office"
     batch_size = 128
     records = []
     torch.manual_seed(0)
-
     # Load data
     train_transform = transforms.Compose(
         [
+            # 随机水平翻转图像，增强泛化能力。
             transforms.RandomHorizontalFlip(),
             transforms.RandomCrop(32, 4),
             transforms.ToTensor(),
@@ -79,7 +77,6 @@ if __name__ == "__main__":
             ),
         ]
     )
-
     valid_transform = transforms.Compose(
         [
             transforms.ToTensor(),
@@ -88,111 +85,102 @@ if __name__ == "__main__":
             ),
         ]
     )
-
     # root变量下需要存放cifar-10-python.tar.gz 文件
     # cifar-10-python.tar.gz可从 "https://www.cs.toronto.edu/~kriz/cifar-10-python.tar.gz" 下载
     train_set = datasets.CIFAR10(root=data_dir, train=True, transform=train_transform, download=True)
-    test_set = datasets.CIFAR10(root=data_dir, train=False, transform=valid_transform, download=True)
-
+    test_set = datasets.CIFAR10(
+        root=data_dir, train=False, transform=valid_transform, download=True)
     # 构建DataLoder
     train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True, num_workers=4)
-    valid_loader = DataLoader(dataset=test_set, batch_size=batch_size, num_workers=4)
-
+    valid_loader = DataLoader(
+        dataset=test_set, batch_size=batch_size, num_workers=4)
     logger = set_logger("classification_cifar10_cnn", use_tb_logger=True)
     #
     # ============================= FusionClassifier =============================
+    # 创建一个名为 FusionClassifier 的集成模型实例
     model = FusionClassifier(
+        # estimator=LeNet5: 基模型是 LeNet5（卷积神经网络，经典用于图像分类）。
+        # n_estimators=n_estimators: 子模型数量（前面设置的是 5）。
+        # cuda=True: 如果有 GPU，就使用 GPU 加速。
+        # 5个子模型 结构上一样，参数初始化可能不同，训练过程中也可能各自学习不同的东西。
         estimator=LeNet5, n_estimators=n_estimators, cuda=True
     )
-
     # Set the optimizer
+    # 设置优化器
     model.set_optimizer("Adam", lr=lr, weight_decay=weight_decay)
-
     # Training
+    #  模型训练
     tic = time.time()
     model.fit(train_loader, epochs=epochs)
     toc = time.time()
+    # 记录训练前后的时间，用来计算训练耗时。
+    # training_time 就是模型训练一共用了多少秒。
     training_time = toc - tic
-
     # Evaluating
     tic = time.time()
     testing_acc = model.evaluate(valid_loader)
     toc = time.time()
     evaluating_time = toc - tic
-
+    # 将本次训练的结果加入 records 列表。
+# 这个 records 就是为后续 display_records(records, logger) 准备的。
     records.append(
         ("FusionClassifier", training_time, evaluating_time, testing_acc)
     )
-
     # ============================= VotingClassifier =============================
     model = VotingClassifier(
         estimator=LeNet5, n_estimators=n_estimators, cuda=True
     )
-
     # Set the optimizer
     model.set_optimizer("Adam", lr=lr, weight_decay=weight_decay)
-
     # Training
     tic = time.time()
     model.fit(train_loader, epochs=epochs)
     toc = time.time()
     training_time = toc - tic
-
     # Evaluating
     tic = time.time()
     testing_acc = model.evaluate(valid_loader)
     toc = time.time()
     evaluating_time = toc - tic
-
     records.append(
         ("VotingClassifier", training_time, evaluating_time, testing_acc)
     )
-
     # ============================= BaggingClassifier =============================
     model = BaggingClassifier(
         estimator=LeNet5, n_estimators=n_estimators, cuda=True
     )
-
     # Set the optimizer
     model.set_optimizer("Adam", lr=lr, weight_decay=weight_decay)
-
     # Training
     tic = time.time()
     model.fit(train_loader, epochs=epochs)
     toc = time.time()
     training_time = toc - tic
-
     # Evaluating
     tic = time.time()
     testing_acc = model.evaluate(valid_loader)
     toc = time.time()
     evaluating_time = toc - tic
-
     records.append(
         ("BaggingClassifier", training_time, evaluating_time, testing_acc)
     )
-
     # ============================= GradientBoostingClassifier =============================
     model = GradientBoostingClassifier(
         estimator=LeNet5, n_estimators=n_estimators, cuda=True
     )
-
     # Set the optimizer
     model.set_optimizer("Adam", lr=lr, weight_decay=weight_decay)
-
     # Training
     tic = time.time()
     # model.fit(train_loader, epochs=epochs)
     model.fit(train_loader, epochs=1)
     toc = time.time()
     training_time = toc - tic
-
     # Evaluating
     tic = time.time()
     testing_acc = model.evaluate(valid_loader)
     toc = time.time()
     evaluating_time = toc - tic
-
     records.append(
         (
             "GradientBoostingClassifier",
@@ -201,27 +189,22 @@ if __name__ == "__main__":
             testing_acc,
         )
     )
-
     # ============================= SnapshotEnsembleClassifier =============================
     model = SnapshotEnsembleClassifier(
         estimator=LeNet5, n_estimators=n_estimators, cuda=True
     )
-
     # Set the optimizer
     model.set_optimizer("Adam", lr=lr, weight_decay=weight_decay)
-
     # Training
     tic = time.time()
     model.fit(train_loader, epochs=epochs)
     toc = time.time()
     training_time = toc - tic
-
     # Evaluating
     tic = time.time()
     testing_acc = model.evaluate(valid_loader)
     toc = time.time()
     evaluating_time = toc - tic
-
     records.append(
         (
             "SnapshotEnsembleClassifier",
@@ -230,27 +213,22 @@ if __name__ == "__main__":
             testing_acc,
         )
     )
-
     # ============================= SoftGradientBoostingClassifier =============================
     model = SoftGradientBoostingClassifier(
         estimator=LeNet5, n_estimators=n_estimators, cuda=True
     )
-
     # Set the optimizer
     model.set_optimizer("Adam", lr=lr, weight_decay=weight_decay)
-
     # Training
     tic = time.time()
     model.fit(train_loader, epochs=epochs)
     toc = time.time()
     training_time = toc - tic
-
     # Evaluating
     tic = time.time()
     testing_acc = model.evaluate(valid_loader)
     toc = time.time()
     evaluating_time = toc - tic
-
     records.append(
         (
             "SoftGradientBoostingClassifier",
@@ -259,8 +237,5 @@ if __name__ == "__main__":
             testing_acc,
         )
     )
-
     # Print results on different ensemble methods
     display_records(records, logger)
-
-
